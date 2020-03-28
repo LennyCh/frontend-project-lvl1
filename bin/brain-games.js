87b99358a292fc9b8f2ee068c9e49e8b6448b693
Lena Chepkasova <lena.chepkasova.95@mail.ru>
2020-03-29 04:13:38 +0500

    step 3

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,2 +1,7 @@
 #!/usr/bin/env node
+const readlineSync = require('readline-sync');
+
 console.log('Welcome to the Brain Games!');
+
+const actual = readlineSync.question('May I have your name? ');
+console.log('Hello, ' + actual + '!');
