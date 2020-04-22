707d6ab17f3976b89c9960fba63260a8e74d9943
Lena Chepkasova <lena.chepkasova.95@mail.ru>
2020-04-22 23:11:24 +0500

    modified babel.config.js

--- babel.config.js
+++ babel.config.js
@@ -1,19 +1,10 @@
 module.exports = {
   presets: [
-    ['@babel/preset-env',
-      {
+    ['@babel/preset-env', {
         targets: {
-          node: 'current',
-        },
+        node: 'current',
       },
-    ],
-  ],
-  plugins: [
-    [
-      '@babel/plugin-proposal-pipeline-operator',
-      {
-        proposal: 'minimal',
-      },
-    ],
+    }],
   ],
 };
+
